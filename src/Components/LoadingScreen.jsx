47c971822066b704c2128ae4dd48faa643d5361e
Linus Toma <linustoma11@hotmail.com>
2024-01-18 19:35:43 +0300

    *

---
+++ src/Components/LoadingScreen.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+
+const LoadingScreen = () => {
+
+    useEffect(() => {
+        const loaderContainer = document.querySelector(".loading-container");
+        const pageContent = document.querySelector(".page-content");
+
+        window.addEventListener("load", () => {
+            loaderContainer.classList.add("hidden");
+            pageContent.classList.add("visible");
+        })
+    })
+
+    return (
+        <>
+            <div className='loading-container'>
+                <svg viewBox='0 0 450 160'>
+                    <text x={("50%")} y={("50%")} dy={(".32rem")} textAnchor='middle' className='loading-text'>
+                        Linus Toma
+                    </text>
+                </svg>
+            </div>
+            <div className='page-content'>
+                <Header />
+            </div>
+        </>
+    );
+};
+
+
+export default LoadingScreen;
---
+++ src/Components/ScrollDownLine.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const ScrollDownLine = () => {
+    return (
+        <div className='scroll-line-div'>
+            <div className='scroll-line-text'>Scroll</div>
+            <svg width="110" height="725" viewBox="0 0 110 725" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M55 0V16.5609V34.5019V53.823V57.5032V141.228C55 141.228 176.5 182.63 55 226.332C-66.5 270.035 55 309.137 55 309.137C55 309.137 176.5 351.919 55 391.482C-66.5 431.044 55 475.666 55 475.666C55 475.666 175.504 515.228 55 558.011C-65.5041 600.793 55 641.275 55 641.275V725" stroke="white"/>
+            </svg>
+        </div>
+    );
+};
+
+
+export default ScrollDownLine;
