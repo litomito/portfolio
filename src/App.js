575ec30bdfb927190de853adc92be41e6d966e8d
Linus Toma <linustoma11@hotmail.com>
2024-03-12 13:52:59 +0100

    *

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import LoadingScreen from "./Components/LoadingScreen";
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 		<div className="App">
 			<LoadingScreen />
 			<SpeedInsights />
+			<Analytics />
 		</div>
 	);
 }
